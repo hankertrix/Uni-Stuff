b68758c03107827ef1105b7c4c6da3905d019122
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-08-25 00:15:31 +0800

    Completed the first lab exercises

---
+++ y2s1/ma2012-introduction-to-mechatronics-system-design/lab/exercise-1/exercise-2-reverse-parking-sensor.ino
@@ -0,0 +1,158 @@
+/*
+Exercise 1-2: Reverse Parking Sensor
+Write a program to perform the following tasks:
+   1. Use the Ultrasonic sensor to sense the distance, D of
+      a car's rear bumper from an obstacle
+      - The sensor is turned on when the Slotted Opto Switch is blocked.
+      - Once the sensor is turned on,
+        * Sound the buzzer
+          + At 1 Hz if D > 40 cm
+          + At 2 Hz if 20 cm < D ≤ 40 cm
+          + Continuously if D ≤ 20 cm
+   2. Repeat Part 1 with the Analog Distance Sensor
+*/
+
+// Assuming circuit 1B is used
+// with the buzzer connected to pin 10
+
+// Define the pin numbers
+#define SLOTTED_OPTO_SWITCH_PIN 5
+#define ULTRASONIC_SENSOR_ECHO_PIN 8
+#define ULTRASONIC_SENSOR_TRIGGER_PIN 9
+#define ANALOG_DISTANCE_SENSOR_PIN A5
+#define PIEZO_BUZZER_PIN 10
+
+// The frequency of the tone to play,
+// which is an E5
+#define BEEP_TONE 659
+
+// The boolean to determine whether to use the analog distance sensor
+#define USE_ANALOG_DISTANCE_SENSOR false
+
+// The setup function to set up the program
+void setup() {
+
+    // Set the pin modes of all the input pins
+    pinMode(ULTRASONIC_SENSOR_ECHO_PIN, INPUT);
+    pinMode(ANALOG_DISTANCE_SENSOR_PIN, INPUT);
+
+    // Set the pin mode of all the output pins
+    pinMode(SLOTTED_OPTO_SWITCH_PIN, OUTPUT);
+    pinMode(PIEZO_BUZZER_PIN, OUTPUT);
+}
+
+// Function to get the distance in cm from the sensor
+float get_distance_in_cm(bool use_analog_distance_sensor) {
+
+    // If the analog distance sensor is used
+    if (use_analog_distance_sensor) {
+
+        // Get the voltage value from the analog distance sensor
+        int voltage_value = analogRead(ANALOG_DISTANCE_SENSOR_PIN);
+
+        // Assume that the distance is more than 10 cm
+        // and use the regression equation to calculate the distance
+        // from the voltage value, which is
+        // 28.987390360228x^(-1.1544716085358)
+        float distance_in_cm =
+            28.987390360288 * pow(voltage_value, -1.1544716085358);
+
+        // Return the distance in cm from the analog distance sensor
+        return distance_in_cm;
+    }
+
+    // Otherwise, use the ultrasonic sensor.
+    // Reset the state of the trigger pin.
+    digitalWrite(ULTRASONIC_SENSOR_TRIGGER_PIN, LOW);
+
+    // Delay for 2 microseconds for the sensor to respond
+    delayMicroseconds(2);
+
+    // Set the trigger pin to high
+    digitalWrite(ULTRASONIC_SENSOR_TRIGGER_PIN, HIGH);
+
+    // Delay for 10 microseconds
+    // so that the ultrasonic sensor will send
+    // out an ultrasonic burst
+    delayMicroseconds(10);
+
+    // Set the trigger pin to low to reset it's state
+    digitalWrite(ULTRASONIC_SENSOR_TRIGGER_PIN, LOW);
+
+    // Read the echo pin for the time taken for the ultrasonic
+    // burst to return
+    int time_taken_in_microseconds = pulseIn(ULTRASONIC_SENSOR_ECHO_PIN, HIGH);
+
+    // Convert the time taken into seconds
+    float time_taken_in_seconds = float(time_taken_in_microseconds) / 1000000;
+
+    // Calculate the distance from the time taken
+    // using the speed of sound in air, which is 343 m/s
+    // or 34300 cm/s.
+    // The distance needs to be divided by 2 because
+    // the time taken is the time for the sound wave to travel
+    // to the object and back to the sensor.
+    float distance_in_cm = time_taken_in_seconds * 343 * 1000 / 2;
+
+    // Return the distance in cm from the ultrasonic sensor
+    return distance_in_cm;
+}
+
+// Function to sound the buzzer at a specified frequency in Hz
+void sound_buzzer(int frequency_in_hz) {
+
+    // Get the total duration of the tone in milliseconds
+    float total_duration_in_milliseconds = 1000.0 / frequency_in_hz;
+
+    // Get the delay between each beep in milliseconds
+    float delay_in_milliseconds = 0.30 * total_duration_in_milliseconds;
+
+    // Get the duration of the beep in milliseconds
+    float beep_duration_in_milliseconds = 0.70 * total_duration_in_milliseconds;
+
+    // Iterate over the frequency in Hz
+    for (int i = 0; i < frequency_in_hz; ++i) {
+
+        // Sound the buzzer for the duration of the beep
+        tone(PIEZO_BUZZER_PIN, BEEP_TONE, beep_duration_in_milliseconds);
+
+        // Stop the note from playing
+        noTone(PIEZO_BUZZER_PIN);
+
+        // Wait for the delay between each beep
+        delay(delay_in_milliseconds);
+    }
+}
+
+// The main loop function
+void loop() {
+
+    // If the slotted opto switch is not blocked,
+    // which means the slotted opto switch is set to low,
+    // exit the function
+    if (digitalRead(SLOTTED_OPTO_SWITCH_PIN) == LOW) {
+        return;
+    }
+
+    // Otherwise, the slotted opto switch is blocked,
+    // which means the slotted opto switch is set to low,
+    // so calculate the distance in cm
+    float distance_in_cm = get_distance_in_cm(USE_ANALOG_DISTANCE_SENSOR);
+
+    // If the distance is more than 40 cm,
+    // sound the buzzer at 1 Hz
+    if (distance_in_cm > 40) {
+        sound_buzzer(1);
+    }
+
+    // If the distance is between 20 cm and 40 cm,
+    // sound the buzzer at 2 Hz
+    else if (distance_in_cm > 20 && distance_in_cm <= 40) {
+        sound_buzzer(2);
+    }
+
+    // Otherwise, sound the buzzer continuously
+    else {
+        sound_buzzer(20);
+    }
+}
